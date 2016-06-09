6e8141eb831399675edd47e01c75083a00b7440f
James Chai <cth451@gmail.com>
2016-06-09 18:45:47 +0800

    Implemented Closed Platform Panel Item

--- src/main/java/tk/cth451/transitrailmod/items/ClosedPlatformPanelItem.java
+++ src/main/java/tk/cth451/transitrailmod/items/ClosedPlatformPanelItem.java
@@ -1,14 +1,24 @@
 package tk.cth451.transitrailmod.items;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import tk.cth451.transitrailmod.TransitRailMod;
+import tk.cth451.transitrailmod.blocks.ClosedPlatformPanelBlock;
+import tk.cth451.transitrailmod.blocks.ClosedPlatformTop;
+import tk.cth451.transitrailmod.init.ModBlocks;
 
 public class ClosedPlatformPanelItem extends Item {
+	
+	private Block panel = ModBlocks.closed_platform_panel_block;
+	private Block top = ModBlocks.closed_platform_top;
 	
 	public ClosedPlatformPanelItem(){
 		super();
@@ -18,6 +28,31 @@ public class ClosedPlatformPanelItem extends Item {
 	
 	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
-		return super.onItemUse(stack, playerIn, worldIn, pos, side, hitX, hitY, hitZ);
+		if (side != EnumFacing.UP) {
+			return false;
+		} else {
+			if (!worldIn.getBlockState(pos).getBlock().isFullBlock()){
+				return false;
+			} else {
+				IBlockState block1 = worldIn.getBlockState(pos.up());
+				IBlockState block2 = worldIn.getBlockState(pos.up(2));
+				IBlockState block3 = worldIn.getBlockState(pos.up(3));
+				boolean canPlace = block1.getBlock().equals(Blocks.air);
+				canPlace = canPlace && block2.getBlock().equals(Blocks.air);
+				canPlace = canPlace && block3.getBlock().equals(Blocks.air);
+				// 3 blocks above must be air
+				
+				if (canPlace) {
+					IBlockState panelState = panel.getDefaultState().withProperty(ClosedPlatformPanelBlock.FACING, playerIn.getHorizontalFacing());
+					worldIn.setBlockState(pos.up(), panelState.withProperty(ClosedPlatformPanelBlock.UPPER, false));
+					worldIn.setBlockState(pos.up(2), panelState.withProperty(ClosedPlatformPanelBlock.UPPER, true));
+					IBlockState topState = top.getDefaultState().withProperty(ClosedPlatformTop.FACING, playerIn.getHorizontalFacing());
+					worldIn.setBlockState(pos.up(3), topState);
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
 	}
 }
