5d18278988c7c6660f62bf8f083862d494689850
James Chai <cth451@gmail.com>
2016-07-01 11:03:15 +0800

    bump version and add damage value to train ticket

--- src/main/java/tk/cth451/transitrailmod/items/TrainTicket.java
+++ src/main/java/tk/cth451/transitrailmod/items/TrainTicket.java
@@ -1,7 +1,16 @@
 package tk.cth451.transitrailmod.items;
 
+import java.util.List;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import tk.cth451.transitrailmod.TransitRailMod;
+import tk.cth451.transitrailmod.blocks.prototype.ArrowSign;
 
 public class TrainTicket extends Item {
 	
@@ -9,5 +18,20 @@ public class TrainTicket extends Item {
 		super();
 		setUnlocalizedName("train_ticket");
 		setCreativeTab(TransitRailMod.tabTransitRail);
+		this.maxStackSize = 1;
+		this.setMaxDamage(199);
+		// max 200 uses
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List tooltip, boolean advanced) {
+		int damage = stack.getItemDamage();
+		tooltip.add(damage + "/" + (this.getMaxDamage() + 1));
+	}
+	
+	public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ)
+	{
+		stack.damageItem(1, playerIn);
+		return true;
 	}
 }
