949647686eb86eab99c8cc09aad59e4ce1fdd60b
James Chai <cth451@gmail.com>
2016-07-13 16:43:50 +0800

    implemented slot transfer

--- src/main/java/tk/cth451/transitrailmod/inventory/TicketMachineContainer.java
+++ src/main/java/tk/cth451/transitrailmod/inventory/TicketMachineContainer.java
@@ -82,6 +82,47 @@ public class TicketMachineContainer extends Container {
 	}
 	
 	@Override
+	public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+		ItemStack itemstack = null;
+		Slot slot = (Slot)this.inventorySlots.get(index);
+		
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if (index == 0) {
+				if (!this.mergeItemStack(itemstack1, 3, 39, true)) {
+					return null;
+				}
+				slot.onSlotChange(itemstack1, itemstack);
+			} else if (index >= 3 && index < 30) {
+				if (!this.mergeItemStack(itemstack1, 30, 39, false)) {
+					return null;
+				}
+			} else if (index >= 30 && index < 39) {
+				if (!this.mergeItemStack(itemstack1, 3, 30, false)) {
+					return null;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 3, 39, false)) {
+				return null;
+			}
+
+			if (itemstack1.stackSize == 0) {
+				slot.putStack((ItemStack)null);
+			} else {
+				slot.onSlotChanged();
+			}
+
+			if (itemstack1.stackSize == itemstack.stackSize) {
+				return null;
+			}
+
+			slot.onPickupFromSlot(playerIn, itemstack1);
+		}
+		return itemstack;
+	}
+	
+	@Override
 	public boolean canMergeSlot(ItemStack p_94530_1_, Slot p_94530_2_) {
 		return p_94530_2_.inventory != this.invOutput && super.canMergeSlot(p_94530_1_, p_94530_2_);
 	}
