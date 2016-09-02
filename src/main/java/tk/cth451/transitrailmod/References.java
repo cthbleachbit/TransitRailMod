9545380dec4b709684567ada1986e1007375d027
James Chai <cth451@gmail.com>
2016-09-02 09:20:21 -0500

    Versioning to 0.10

--- src/main/java/tk/cth451/transitrailmod/References.java
+++ src/main/java/tk/cth451/transitrailmod/References.java
@@ -6,5 +6,5 @@ public final class References {
 	public static final String MOD_ID = "transitrailmod";
 	public static final String MOD_NAME = "Transit Rail Mod";
 	public static final String SERVER_PROXY_CLASS = "tk.cth451.transitrailmod.proxy.CommonProxy";
-	public static final String VERSION = "0.10-dev";
+	public static final String VERSION = "0.10";
 }
