9f88a62c278759b63f64bae6da0885f454cd124d
Tianhao Chai <cth451@gmail.com>
2020-08-07 03:18:10 -0500

    make nbt keys constant

--- src/main/java/me/cth451/transit/items/FareCard.java
+++ src/main/java/me/cth451/transit/items/FareCard.java
@@ -16,28 +16,32 @@ import net.minecraft.world.World;
 import java.util.List;
 
 public class FareCard extends Item {
+    public static final String KEY_BALANCE = "balance";
+    public static final String KEY_IN_USE = "in_use";
+
     public FareCard(Settings settings) {
         super(settings.group(ItemGroup.TRANSPORTATION).maxCount(1));
     }
 
     @Override
     public void onCraft(ItemStack stack, World world, PlayerEntity player) {
-        stack.getOrCreateTag().putBoolean("in_use", false);
+        stack.getOrCreateTag().putBoolean(KEY_IN_USE, false);
     }
 
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-        int balance = stack.getOrCreateTag().getInt("balance");
+        int balance = stack.getOrCreateTag().getInt(KEY_BALANCE);
+        stack.getOrCreateTag().putInt(KEY_BALANCE, balance);
         if (balance == -1) {
             tooltip.add(new TranslatableText("item.transit.fare_card.unlimited_tooltip"));
         } else {
             if (balance < 0) {
-                stack.getOrCreateTag().putInt("balance", 0);
+                stack.getOrCreateTag().putInt(KEY_IN_USE, 0);
                 balance = 0;
             }
             tooltip.add(new TranslatableText("item.transit.fare_card.balance_tooltip", balance));
         }
-        if (stack.getOrCreateTag().getBoolean("in_use")) {
+        if (stack.getOrCreateTag().getBoolean(KEY_IN_USE)) {
             tooltip.add(new TranslatableText("item.transit.fare_card.in_use_tooltip"));
         }
     }
@@ -48,8 +52,8 @@ public class FareCard extends Item {
         if (context.getWorld().getBlockState(context.getBlockPos()).getBlock().getClass() != GlassBlock.class) {
             return ActionResult.PASS;
         }
-        int balance = context.getStack().getOrCreateTag().getInt("balance");
-        boolean in_use = context.getStack().getOrCreateTag().getBoolean("in_use");
+        int balance = context.getStack().getOrCreateTag().getInt(KEY_BALANCE);
+        boolean in_use = context.getStack().getOrCreateTag().getBoolean(KEY_IN_USE);
 
         // Handle unlimited pass or someone in creative mode
         if (balance == -1 || context.getPlayer().isCreative()) {
@@ -65,13 +69,13 @@ public class FareCard extends Item {
         }
 
         if (in_use) {
-            context.getStack().getOrCreateTag().putInt("balance", balance - 1);
-            context.getStack().getOrCreateTag().putBoolean("in_use", false);
+            context.getStack().getOrCreateTag().putInt(KEY_BALANCE, balance - 1);
+            context.getStack().getOrCreateTag().putBoolean(KEY_IN_USE, false); 
             if (context.getWorld().isClient()) {
                 context.getPlayer().playSound(SoundEvents.BLOCK_NOTE_BLOCK_BELL, 1.0F, 1.0F);
             }
         } else {
-            context.getStack().getOrCreateTag().putBoolean("in_use", true);
+            context.getStack().getOrCreateTag().putBoolean(KEY_IN_USE  , true);
             if (context.getWorld().isClient()) {
                 context.getPlayer().playSound(SoundEvents.BLOCK_NOTE_BLOCK_BELL, 1.0F, 2.0F);
             }
